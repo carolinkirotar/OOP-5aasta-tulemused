812ee548e272ebbc5d47aab8916e628eef7d45e5
Carolin <carolin@Carolins-MBP.lan>
2023-10-22 11:53:50 +0300

    Eksam ja kontrolltööd lisatud.

---
+++ Kontrolltööde joonised/Kontrolltöö2.py
@@ -0,0 +1,138 @@
+import numpy as np 
+import pandas as pd 
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib
+import researchpy as rp
+import scipy.stats as stats
+import statsmodels.api as sm
+import statsmodels.stats.multicomp as mc
+from statsmodels.formula.api import ols
+
+
+# Dataframe 
+
+df = pd.read_csv('/Users/carolin/Documents/Magister/AndmedRühmagaVALMIS.csv', decimal=',')
+df_copy = df
+
+df_copy = df_copy.replace('-', '0', regex=True)
+
+cols = df.columns
+df_copy[cols[1:]] = df_copy[cols[1:]].apply(pd.to_numeric, errors='coerce')
+
+df_copy['aasta'] = df_copy['aasta'].astype(str)
+
+focused_row = "Kontrolltöö2"
+
+# -------------------------------------------------------------
+
+sns.set_style("whitegrid", {'grid.color': '.8'})
+plt.subplots(figsize=(10, 8))
+
+pal = sns.color_palette("magma")
+print(pal.as_hex())
+# VLAG --> ['#6e90bf', '#aab8d0', '#e4e5eb', '#f2dfdd', '#d9a6a4', '#c26f6d']
+# MAGMA --> ['#221150', '#5f187f', '#982d80', '#d3436e', '#f8765c', '#febb81']
+
+ax = sns.violinplot(data=df_copy, x="aasta", y=focused_row, inner="quart", linewidth=1, scale="count", color='#f8765c')
+plt.setp(ax.collections, alpha=.4)
+
+box_plot = sns.boxplot(data=df_copy, x="aasta", y=focused_row,
+                       boxprops={'zorder': 2, 'fill': None}, width=0.3, showmeans=True,
+                       meanprops={"marker":"o", "markerfacecolor":"red",
+                                  "markeredgecolor":"black","markersize":"15"})   
+    
+# -----------------------------------------------------------
+
+# Mann-Whitney U-test
+first_year = df_copy[df_copy['aasta'] == '2018']
+second_year = df_copy[df_copy['aasta'] == '2019']
+third_year = df_copy[df_copy['aasta'] == '2020']
+fourth_year = df_copy[df_copy['aasta'] == '2021']
+fifth_year1 = df_copy[df_copy['aasta'] == '2022.1']
+fifth_year2 = df_copy[df_copy['aasta'] == '2022.2']
+
+between18and19 = stats.mannwhitneyu(x=first_year[focused_row], y=second_year[focused_row], alternative = 'two-sided')
+between18and20 = stats.mannwhitneyu(x=first_year[focused_row], y=third_year[focused_row], alternative = 'two-sided')
+between18and21 = stats.mannwhitneyu(x=first_year[focused_row], y=fourth_year[focused_row], alternative = 'two-sided')
+between18and221 = stats.mannwhitneyu(x=first_year[focused_row], y=fifth_year1[focused_row], alternative = 'two-sided')
+between18and222 = stats.mannwhitneyu(x=first_year[focused_row], y=fifth_year2[focused_row], alternative = 'two-sided')
+between19and20 = stats.mannwhitneyu(x=second_year[focused_row], y=third_year[focused_row], alternative = 'two-sided')
+between19and21 = stats.mannwhitneyu(x=second_year[focused_row], y=fourth_year[focused_row], alternative = 'two-sided')
+between19and221 = stats.mannwhitneyu(x=second_year[focused_row], y=fifth_year1[focused_row], alternative = 'two-sided')
+between19and222 = stats.mannwhitneyu(x=second_year[focused_row], y=fifth_year2[focused_row], alternative = 'two-sided')
+between20and21 = stats.mannwhitneyu(x=third_year[focused_row], y=fourth_year[focused_row], alternative = 'two-sided')
+between20and221 = stats.mannwhitneyu(x=third_year[focused_row], y=fifth_year1[focused_row], alternative = 'two-sided')
+between20and222 = stats.mannwhitneyu(x=third_year[focused_row], y=fifth_year2[focused_row], alternative = 'two-sided')
+between21and221 = stats.mannwhitneyu(x=fourth_year[focused_row], y=fifth_year1[focused_row], alternative = 'two-sided')
+between21and222 = stats.mannwhitneyu(x=fourth_year[focused_row], y=fifth_year2[focused_row], alternative = 'two-sided')
+between221and222 = stats.mannwhitneyu(x=fifth_year1[focused_row], y=fifth_year2[focused_row], alternative = 'two-sided')
+
+print("2018 ja 2019: " + str(between18and19))
+print("2018 ja 2020: " + str(between18and20) + str(round(between18and20.pvalue, 4)))
+print("2018 ja 2021: " + str(between18and21))
+print("2018 ja 2022.1: " + str(between18and221))
+print("2018 ja 2022.2: " + str(between18and222))
+print("2019 ja 2020: " + str(between19and20))
+print("2019 ja 2021: " + str(between19and21))
+print("2019 ja 2022.1: " + str(between19and221))
+print("2019 ja 2022.2: " + str(between19and222) + " " + str(round(between19and222.pvalue, 4)))
+print("2020 ja 2021: " + str(between20and21) + " " + str(round(between20and21.pvalue, 4)))
+print("2020 ja 2022.1: " + str(between20and221) + " " + str(round(between20and221.pvalue, 4)))
+print("2020 ja 2022.2: " + str(between20and222) + " " + str(round(between20and222.pvalue, 4)))
+print("2021 ja 2022.1: " + str(between21and221))
+print("2021 ja 2022.2: " + str(between21and222))
+print("2022.1 ja 2022.2: " + str(between221and222))
+
+# --------------------------------------------------------------------------
+# Kruskal-Wallis test
+
+tulemus = stats.kruskal(df_copy[focused_row][df_copy['aasta'] == '2018'],
+               df_copy[focused_row][df_copy['aasta'] == '2019'],
+               df_copy[focused_row][df_copy['aasta'] == '2020'],
+               df_copy[focused_row][df_copy['aasta'] == '2021'],
+               df_copy[focused_row][df_copy['aasta'] == '2022.1'],
+               df_copy[focused_row][df_copy['aasta'] == '2022.2']         )
+print("\n")
+print("Kruskali test:" + str(tulemus))
+print(round(tulemus.pvalue, 4))
+
+# --------------------------------------------------------------------------
+
+# näidata keskmist ja sd keskel
+means = np.array(df_copy.groupby(['aasta'])[focused_row].mean())
+stds = np.array(df_copy.groupby(['aasta'])[focused_row].std())
+summary = rp.summary_cont(df_copy[focused_row].groupby(df_copy['aasta']))
+conf = np.array(summary['95% Conf.'])
+interval = np.array(summary['Interval'])
+
+for i in range(len(means)):
+    text = ' μ={:.2f}\n σ={:.2f}\n 95% Conf=\n({:.2f}, {:.2f})'.format(means[i], stds[i], conf[i], interval[i])
+    box_plot.annotate(text, xy=(i, means[i]), xytext=(i+0.04, means[i]-2.0), ha='left', fontsize='8',
+                      bbox=dict(facecolor='white', edgecolor='grey', boxstyle='round,pad=1'))
+
+
+# ---------------------------------------------------------------
+
+# näidata keskmise ühendusjooni
+mean = df_copy.groupby(['aasta'])[focused_row].mean()
+box_plot.plot(mean.values, 'r-o', linewidth=3, alpha=0.5)
+
+# ---------------------------------------------------------------
+
+# näidata iga aasta juures n arvu
+nobs = df_copy['aasta'].value_counts(sort=False).values
+nobs = [str(x) for x in nobs.tolist()]
+nobs = ["(n: " + i + ")" for i in nobs]
+
+pos = range(len(nobs))
+for tick, label in zip(pos, box_plot.get_xticklabels()):
+   box_plot.text(pos[tick], -1.4, nobs[tick],
+            ha='center',
+            size='small')
+
+
+plt.ylim(-0.3, 17)
+plt.ylabel('Kontrolltöö 2', fontweight='bold')
+plt.xlabel('Õppeaasta', fontweight='bold', labelpad=25)
+plt.show()
